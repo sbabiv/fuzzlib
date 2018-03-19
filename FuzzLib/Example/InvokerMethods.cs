6b7451f6629e26e662f67859a28e72d7d8270ec5
WILDBERRIES\sbabiv <sbabiv@wildberries.ru>
2018-03-19 13:40:50 +0300

    add examples

---
+++ FuzzLib/Example/InvokerMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FuzzLib.TemplateInvokerMethods;
+
+namespace Example
+{
+    public class HelloWorldMethod : InvokerMethodBase<string>, ITemplateInvokerMethod
+    {
+        [InvokerMethod("HelloWorld")]
+        public string InvokeMethod(params string[] args)
+        {
+            return $"hello world {Guid.NewGuid()}";
+        }
+    }
+
+    public class RemoveLink : InvokerMethodBase<string>, ITemplateInvokerMethod
+    {
+        private const string URL = "http://www.car-shop.{0}/unsubscribe/{1}/{2}";
+
+        [InvokerMethod("RemoveLink")]
+        public string InvokeMethod(params string[] args)
+        {
+            var key = String.Join(":", args);
+            if (_cache.ContainsKey(key))
+                return _cache[key];
+
+            var result = string.Format(URL, args[0], args[1], args[2]);
+            _cache.TryAdd(key, result);
+
+            return result;
+        }
+    }
+}
---
+++ FuzzLib/Example/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+using FuzzLib.Binary;
+using FuzzLib.Data;
+using FuzzLib.Functions;
+using FuzzLib.Parser;
+using FuzzLib.TemplateInvokerMethods;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmplFilePath = Path.Combine(Environment.CurrentDirectory, @"Statics\Template.txt");
+            var dataFilePath = Path.Combine(Environment.CurrentDirectory, @"Statics\Data.xml");
+
+            //read template 
+            var content = File.ReadAllText(tmplFilePath);
+            //read message
+            var xml = XDocument.Load(dataFilePath).Element("item");
+
+            //convert message to Dictionary
+            var converter = new LinqDataConverter();
+            var data = converter.ToData(xml);
+
+            //get custom methods
+            var methodsMapper = new InvokeMethodsMapper();
+            var parser = new TemplateParser(new FunctionsContainer());
+            //set custom methods for parser
+            parser.SetMethods(methodsMapper.GetMethods().ToArray());
+
+            //create and compile template 
+            var factory = new BinaryTemplatesFactory(parser);
+            var template = factory.Compile(content, true);
+            template.SetHandlers(() => methodsMapper.GetInvokers());
+
+            //render content
+            var output = template.Render(data);
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
